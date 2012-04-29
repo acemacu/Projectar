ea9c32c292a5d45e4929e0d2ba01e9f5390ddada
Ana Martinez <acemacu@gmail.com>
2012-04-28 21:22:16 -0700

    Moved to ruby 1.9 and fixed encoding issues

--- app/controllers/csv_import_controller.rb
+++ app/controllers/csv_import_controller.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'iconv'
 
 class CsvImportController < ApplicationController
   def csv_import
@@ -6,39 +7,39 @@ class CsvImportController < ApplicationController
   
   def house_hold_summary
     Side.all.each {|r| r.destroy}
-    @parsed_file = CSV::Reader.parse(params[:dump][:file])
-    @parsed_file.each do |row|
+    @data = (params[:dump][:file]).read
+    CSV.parse(@data) do |row|
       save_side(row[2]) if row[2] != nil
       save_household(row[0], row[2]) if row[0] != nil
     end
-    import_guests(params[:dump][:guests])
+    import_guests(params[:dump][:guests].read)
     redirect_to('/csv_import', :notice => "Imported successfully") 
   end
   
   def import_guests(guests_file)
     Person.all.each {|r| r.destroy}
-    @parsed_file = CSV::Reader.parse(guests_file)
-    @parsed_file.each do |row|
+    CSV.parse(guests_file) do |row|
       save_guests(row)
     end
   end
   
   def save_side(side_name)
     side = Side.new
-    side.name = side_name.to_s
-    side.save if Side.find_by_name(side_name.to_s) == nil
+    side.name = Iconv.iconv('utf-8', 'iso8859-1', side_name).first
+    side.save if Side.find_by_name(side_name) == nil
   end
   
   def save_household(household, side_name)
     puts household
-    side = Side.find_by_name(side_name.to_s)
-    side.households.create(:name => household.to_s)
+    side = Side.find_by_name(side_name)
+    name = Iconv.iconv('utf-8', 'iso8859-1', household).first
+    side.households.create(:name => name)
   end
   
   def save_guests(people)
     guest = Person.new
-    guest.first_name = people[0]
-    guest.lastname = people[1]
+    guest.first_name = Iconv.iconv('utf-8', 'iso8859-1', people[0]).first
+    guest.lastname = Iconv.iconv('utf-8', 'iso8859-1', people[1]).first
     guest.household_id = find_household_id(people[2])
     people[5] == "Yes" ? guest.child = true : guest.child = false
     guest.save if guest.household_id
