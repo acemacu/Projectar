10f33eb0c1af297bbefe09cd7b53033ac399b823
Ana Martinez <acemacu@gmail.com>
2012-04-28 21:57:54 -0700

    Fixed strings error

--- db/migrate/20120425025647_add_colum_to_households.rb
+++ db/migrate/20120425025647_add_colum_to_households.rb
@@ -1,6 +1,6 @@
 class AddColumToHouseholds < ActiveRecord::Migration
   def self.up
-    add_column :households, :name, :strings
+    add_column :households, :name, :string
   end
 
   def self.down
